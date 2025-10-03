10ee5caa8e83e00801189f4b1449a8c03bd67122
SamitDharia <smtdharia@gmail.com>
2025-10-03 14:01:19 +0100

    update

---
+++ scripts/error_watchdog.cmd
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+pushd %~dp0\..
+if not exist venv\Scripts\python.exe (
+  echo Virtualenv missing. Please run python -m venv venv && exit /b 1
+)
+"venv\Scripts\python.exe" scripts\error_watchdog.py %*
+popd
