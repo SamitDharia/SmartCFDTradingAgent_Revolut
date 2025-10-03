8a9d8091b7ea619f223b3c922d4fc51576097933
SamitDharia <smtdharia@gmail.com>
2025-10-03 14:03:31 +0100

    Update test_telegram.cmd

--- scripts/test_telegram.cmd
+++ scripts/test_telegram.cmd
@@ -4,5 +4,5 @@ pushd %~dp0\..
 if not exist venv\Scripts\python.exe (
   echo Virtualenv missing. Please run python -m venv venv && exit /b 1
 )
-"venv\Scripts\python.exe" scripts\test_telegram.py %*
+"venv\Scripts\python.exe" scripts\\telegram_ping.py %*
 popd
