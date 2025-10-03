52dc8a11530cda160f0462f4108318c01dff7806
SamitDharia <smtdharia@gmail.com>
2025-10-03 14:40:52 +0100

    Update launch_dashboard.cmd

--- launch_dashboard.cmd
+++ launch_dashboard.cmd
@@ -4,5 +4,5 @@ pushd %~dp0
 if not exist venv\Scripts\python.exe (
   echo Virtualenv missing. Please run python -m venv venv && exit /b 1
 )
-start "SmartCFD Dashboard" cmd /k "cd /d %~dp0 && call venv\Scripts\activate && streamlit run apps\dashboard.py"
+start "SmartCFD Dashboard" cmd /k "cd /d %~dp0 && call venv\Scripts\activate.bat && streamlit run apps\dashboard.py"
 popd
