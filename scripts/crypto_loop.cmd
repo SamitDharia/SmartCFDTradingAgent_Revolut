c57c55262bc65ca29a692d83c410151991eb2560
SamitDharia <smtdharia@gmail.com>
2025-10-03 16:20:56 +0100

    Update crypto_loop.cmd

--- scripts/crypto_loop.cmd
+++ scripts/crypto_loop.cmd
@@ -5,10 +5,10 @@ if not exist venv\Scripts\python.exe (
   echo Virtualenv missing. Please run python -m venv venv && exit /b 1
 )
 set SKIP_SSL_VERIFY=1
-echo SKIP_SSL_VERIFY=%SKIP_SSL_VERIFY%
 set TELEGRAM_SKIP_VERIFY=1
 set PYTHONHTTPSVERIFY=0
 set CURL_CA_BUNDLE=
 set REQUESTS_CA_BUNDLE=
+set USE_ALPACA_CRYPTO=1
 "venv\Scripts\python.exe" -m SmartCFDTradingAgent.pipeline --config configs\crypto.yml --profile crypto_1h %*
 popd
